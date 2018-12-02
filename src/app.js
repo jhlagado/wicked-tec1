8482ad4ab7195c86db7ea0581790bf7de4bf7864
John Hardy <jh@lagado.com>
2018-12-02 19:28:18 +1100

    adding props

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ const byteToHex = (byte) => (byte & 0xff).toString(16).padStart(2, '0');
 const wordToHex = (word) => (word & 0xffff).toString(16).padStart(4, '0');
 
 const keyMap = {
-  Digit0: 0x00, Digit1: 0x01, Digit2: 0x02, Digit3: 0x03,
-  Digit4: 0x04, Digit5: 0x05, Digit6: 0x06, Digit7: 0x07,
-  Digit8: 0x08, Digit9: 0x09,
-  KeyA: 0x0A, KeyB: 0x0B, KeyC: 0x0C, KeyD: 0x0D,
-  KeyE: 0x0E, KeyF: 0x0F,
-  Space: 0x13, Tab: 0x13,
-  Enter: 0x12, Minus: 0x11,
-  ArrowDown: 0x11, ArrowUp: 0x10,
+    Digit0: 0x00, Digit1: 0x01, Digit2: 0x02, Digit3: 0x03,
+    Digit4: 0x04, Digit5: 0x05, Digit6: 0x06, Digit7: 0x07,
+    Digit8: 0x08, Digit9: 0x09,
+    KeyA: 0x0A, KeyB: 0x0B, KeyC: 0x0C, KeyD: 0x0D,
+    KeyE: 0x0E, KeyF: 0x0F,
+    Space: 0x13, Tab: 0x13,
+    Enter: 0x12, Minus: 0x11,
+    ArrowDown: 0x11, ArrowUp: 0x10,
 };
 
 export function initApp() {
@@ -22,8 +22,6 @@ export function initApp() {
 
         init: function (event) {
             this.el = event.currentTarget;
-            this.el.attachShadow({ mode: 'open' });
-            observeProperties(this, this.el);
 
             this._digits = 0;
             this._segments = 0;
@@ -53,13 +51,11 @@ export function initApp() {
             document.removeEventListener("keydown", this.handleKeyDown.bind(this));
         },
 
-        onattributechanged(event) {
-            console.log('on attr changed');
-        },
-
         onclick(event) {
             console.log('ON CLICK!!');
-            this.el.x++;
+            if (event.shiftlKey) {
+                this.worker.postMessage({ type: 'EXIT' });
+            }
         },
 
         onkeydown(event) {
@@ -79,8 +75,8 @@ export function initApp() {
                 if (event.shiftKey) {
                     keyCode = keyCode | 0x80;
                 }
-                this.worker.postMessage({ type: 'SET_INPUT_VALUE', port: 0, value: keyCode});
-                this.worker.postMessage({ type: 'NMI'});
+                this.worker.postMessage({ type: 'SET_INPUT_VALUE', port: 0, value: keyCode });
+                this.worker.postMessage({ type: 'NMI' });
                 event.preventDefault();
             }
             else {
@@ -88,30 +84,140 @@ export function initApp() {
             }
         },
 
+        get html() {
+            return hyperHTML.bind(this.el);
+        },
+
         render() {
             this.html`!
 <slot></slot>!
 <p>digits ${this._digits}</p>
 <p>segments ${this._segments}</p>
-<p>I am rando ${this._rando}</p>`;
+<p>I am rando ${this._rando}</p>
+<div id="seven" is="seven-seg-display" data=${{ digits: this._digits, segments: this._segments }} digits=${{ digits: this._digits }}
+    segments=${this._segments}>7 Segment display</div>
+            `;
+            // const el = document.getElementById('seven');
+            // el.digits = 1;
+        },
+
+    });
+
+
+    ////////////////////////////////////////////////////////////////
+
+
+
+
+    wickedElements.define('[is="seven-seg-display"]', {
+
+        init: function (event) {
+            this.el = event.currentTarget;
+            this._data = {};
+            const context = this;
+
+            Object.defineProperty(this, 'data', {
+
+                get() {
+                    return context._data
+                },
+            });
+
+            Object.defineProperty(this.el, 'data', {
+
+                get() {
+                    return context._data
+                },
+
+                set(value) {
+                    const oldValue = context._data;
+                    context._data = value;
+                    const props = context.observedProperties || Object.keys(value);
+                    props.forEach((prop) => {
+                        const oldProp = oldValue[prop];
+                        const newProp = value[prop];
+                        if (oldProp !== newProp) {
+                            context.propertyChanged(prop, newProp, oldProp)
+                        }
+                    });
+                }
+            });
+
+            this.requestRender = debounceRender(this);
+            this.requestRender();
+        },
+
+        onconnected(event) {
+            console.log('on connected');
+        },
+
+        ondisconnected(event) {
+            console.log('on disconnected');
         },
 
         get html() {
-            return hyperHTML.bind(this.el.shadowRoot);
+            return hyperHTML.bind(this.el);
         },
 
         get observedProperties() {
-            return ['x', 'y'];
+            return ['data', 'digits', 'segments'];
         },
 
         propertyChanged(prop, newValue, oldValue) {
             console.log(`property changed ${prop} ${newValue} ${oldValue} `);
-            this.render();
-        }
+            this.requestRender();
+        },
+
+        // onattributechanged({attributeName, newValue, oldValue}) {
+        //     console.log(`attribute changed`, {attributeName, newValue, oldValue});
+        //     this.requestRender();
+        // },
+
+        render() {
+            this.html`
+<p>digits ${this.data.digits}</p>
+<p>segments ${this.data.segments}</p>
+            `;
+        },
+
     });
+}
 
-    const elem = document.getElementById('wicked');
-    elem.x = 20;
-    elem.y = [...[1, 2, 3]];
+function withProps(definition) {
 
+    return {
+        init(event) {
+
+            Object.assign(this, definition);
+            this.init(event);
+
+            const context = this;
+            Object.defineProperty(this, 'data', {
+
+                get() {
+                    return context._data
+                }
+            });
+
+            Object.defineProperty(this.el, 'data', {
+
+                get() {
+                    return context._data
+                },
+
+                set(value) {
+                    const oldValue = context._data;
+                    context._data = value;
+                    const props = context.observedProperties || Object.keys(value);
+                    props.forEach((prop) => {
+                        const oldProp = oldValue[prop];
+                        const newProp = value[prop];
+                        if (oldProp !== newProp) {
+                            context.propertyChanged(prop, newProp, oldProp)
+                        }
+                    });
+                }
+            });
+        }
+    };
 }
