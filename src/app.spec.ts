50928f21206445650af63de49954b52ccdb673d4
John Hardy <jh@lagado.com>
2018-12-26 21:14:16 +1100

    fixing travis deploy

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,5 +1,9 @@
-it('should reset id', () => {
-	const expected = 0;
-	const actual = 0;
-	expect(expected).toEqual(actual);
+describe('wicked tec-1', () => {
+
+	test('should reset id', () => {
+		const expected = 0;
+		const actual = 0;
+		expect(expected).toEqual(actual);
+	});
+
 });
